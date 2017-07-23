7a1725574ea50d55745ddb80299ae3b6c8abe379
Dashie <dashie@sigpipe.me>
2017-07-23 18:51:11 +0200

    Add cron job for Transcoding Watchdog Job who cannot be added to queue, like, redis unavailable, will be re- added to the queue, if: not present, and track.ready=false.

--- pkg/cron/cron.go
+++ pkg/cron/cron.go
@@ -2,7 +2,9 @@ package cron
 
 import (
 	"github.com/gogits/cron"
-	//log "gopkg.in/clog.v1"
+	log "gopkg.in/clog.v1"
+	"time"
+	"dev.sigpipe.me/dashie/reel2bits/workers"
 )
 
 var c = cron.New()
@@ -10,11 +12,18 @@ var c = cron.New()
 // NewContext initiate the cron thing
 func NewContext() {
 	var (
-	//entry *cron.Entry
-	//err   error
+		entry *cron.Entry
+		err   error
 	)
 
 	// Add crons here
+
+	log.Trace("Enabling TranscodingWatchdog")
+	entry, err = c.AddFunc("Transcoding Watchdog", "@every 5m", workers.TranscodingWatchdog)
+	if err != nil {
+		log.Fatal(2, "Cron.(transcoding watchdog): %v", err)
+	}
+	entry.Next = time.Now().Add(1 * time.Minute)
 
 	c.Start()
 }
---
+++ workers/transcoding_watchdog.go
@@ -0,0 +1,104 @@
+package workers
+
+import (
+	"dev.sigpipe.me/dashie/reel2bits/pkg/sync"
+	log "gopkg.in/clog.v1"
+	"dev.sigpipe.me/dashie/reel2bits/models"
+	"github.com/RichardKnop/machinery/v1/tasks"
+	"strings"
+	"fmt"
+)
+
+var taskStatusTable = sync.NewStatusTable()
+const (
+	_TRANSCODING_WATCHDOG = "transcoding_watchdog"
+)
+
+// TranscodingWatchdog take care to add un-ready tracsk to the worker queue if not ready and not found in it
+// The track statuses are not updated, only by the worker when processed
+// I don't think we really care to set the flag to "processing" "wait" etc. since this watchdog is mainly
+// only for the case of redis unavailable when the track is uploaded, and this sould not happens lot of times.
+func TranscodingWatchdog() {
+	if taskStatusTable.IsRunning(_TRANSCODING_WATCHDOG) {
+		return
+	}
+	taskStatusTable.Start(_TRANSCODING_WATCHDOG)
+	defer taskStatusTable.Stop(_TRANSCODING_WATCHDOG)
+
+	log.Trace("Running cron: TranscodingWatchdog")
+
+	// First case : empty workers list
+	server, err := CreateServer()
+	if err != nil {
+		log.Error(2, "Cannot initiate the worker connection, please retry again.")
+	}
+
+	queue_transcoding_infos, _ := server.GetBroker().GetPendingTasks("reel2bits_queue")
+
+	// get all un-ready tracks
+	tracks, err := models.GetNotReadyTracks()
+	if err != nil {
+		log.Error(2, "Cannot get un-ready tracks: %s", err)
+	}
+	if len(tracks) <= 0 {
+		return
+	}
+
+	if len(queue_transcoding_infos) <= 0 {
+		log.Info("Workers queue is empty, checking for un-ready tracks.")
+		log.Info("Found %d un-ready tracks to process.", len(tracks))
+		// Add them to the worker queue
+		for _, t := range tracks {
+			sig := &tasks.Signature{
+				Name: "TranscodeAndFetchInfos",
+				Args: []tasks.Arg{{Type: "int64", Value: t.ID, }, },
+			}
+
+			_, err = server.SendTask(sig)
+			if err != nil {
+				log.Error(2, "Cannot push the worker job for %d, please retry again. %s", t.ID, err)
+			}
+		}
+		return
+	}
+
+	// Second case : we have some worker elements to process, but there is un-processed tracks not in list
+	for _, t := range tracks {
+		trackFound := false
+		// check if we find the track in the queue
+		for _, qi := range queue_transcoding_infos {
+			arg := qi.Args[0]
+
+			var argValue int64
+
+			if strings.HasPrefix(fmt.Sprintf("%T", arg.Value), "float") {
+				n, ok := arg.Value.(float64)
+				if !ok {
+					log.Error(2, "Cannot convert to float64")
+					continue
+				}
+				argValue = int64(n)
+			}
+
+			if t.ID == argValue {
+				trackFound = true
+			}
+		}
+
+		// Found it ?
+		if !trackFound {
+			log.Trace("Cannot found track %d in transcoding queue", t.ID)
+			// Add it
+			sig := &tasks.Signature{
+				Name: "TranscodeAndFetchInfos",
+				Args: []tasks.Arg{{Type: "int64", Value: t.ID, }, },
+			}
+
+			_, err = server.SendTask(sig)
+			if err != nil {
+				log.Error(2, "Cannot push the worker job for %d, please retry again. %s", t.ID, err)
+			}
+		}
+	}
+
+}
