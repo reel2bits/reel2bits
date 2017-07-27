c0642712dd9104cf9ca98d4b2d2cfbc6801b26b6
Dashie <dashie@sigpipe.me>
2017-07-27 13:33:02 +0200

    Test: register

--- integrations/user_test.go
+++ integrations/user_test.go
@@ -22,6 +22,7 @@ func TestLogin(t *testing.T) {
 		title := fmt.Sprintf("<title>%s - %s</title>", i18n.Tr("en", "settings.title"), setting.AppName)
 
 		So(string(resp.Body), ShouldContainSubstring, title)
+		So(string(resp.Body), ShouldContainSubstring, "userA")
 	})
 }
 
@@ -31,12 +32,45 @@ func TestLogout(t *testing.T) {
 
 		session := loginUser(t, "userA")
 
-		// then test settings page
 		req := NewRequest(t, "GET", "/user/logout")
 		resp := session.MakeRequest(t, req, http.StatusFound)
 
 		var found = "<a href=\"/\">Found</a>"
 
 		So(string(resp.Body), ShouldContainSubstring, found)
+	})
+}
+
+func TestRegister(t *testing.T) {
+	Convey("Test register user C", t, func() {
+		prepareTestEnv(t)
+
+		req := NewRequest(t, "GET", "/user/register")
+		resp := MakeRequest(t, req, http.StatusOK)
+
+		// For CSRF
+		doc := NewHTMLParser(t, resp.Body)
+
+		// Register
+		req = NewRequestWithValues(t, "POST", "/user/register", map[string]string{
+			"_csrf":     doc.GetCSRF(),
+			"user_name": "userC",
+			"email": "userc@example.com",
+			"password": "password",
+			"repeat": "password",
+		})
+		MakeRequest(t, req, http.StatusFound)
+
+		// Login
+		session := loginUser(t, "userC")
+
+		// then test settings page
+		req = NewRequest(t, "GET", "/user/settings")
+		resp = session.MakeRequest(t, req, http.StatusOK)
+
+		title := fmt.Sprintf("<title>%s - %s</title>", i18n.Tr("en", "settings.title"), setting.AppName)
+
+		So(string(resp.Body), ShouldContainSubstring, title)
+		So(string(resp.Body), ShouldContainSubstring, "userC")
 	})
 }
