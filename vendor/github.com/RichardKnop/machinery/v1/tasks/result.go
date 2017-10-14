9e884b0103062eb5b94ad450c457ba705810900e
Dashie <dashie@sigpipe.me>
2017-10-14 14:47:56 +0200

    bye bye

--- vendor/github.com/RichardKnop/machinery/v1/tasks/result.go
+++
@@ -1,7 +0,0 @@
-package tasks
-
-// TaskResult represents an actual return value of a processed task
-type TaskResult struct {
-	Type  string      `bson:"type"`
-	Value interface{} `bson:"value"`
-}
