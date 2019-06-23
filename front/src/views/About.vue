940c379d3cfc923d77b1eb6a0620ce4daa2173bd
Dashie <dashie@sigpipe.me>
2019-06-23 21:53:44 +0200

    Add informations on the about page

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>Welcome on instance: {{ instanceName }}</h2>
+    <div>
+      <h3>Description</h3>
+      {{ instanceDescription }}
+    </div>
+    <div>
+      <h3>Terms of service</h3>
+      {{ instanceTos }}
+    </div>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      instanceName: state => state.instance.name,
+      instanceDescription: state => state.instance.description,
+      instanceTos: state => state.instance.tos
+    })
+  }
+}
+</script>
