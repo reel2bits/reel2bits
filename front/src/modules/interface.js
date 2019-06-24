3fbb9a467dcbae2f7d76f8fd062f0a0295ff6e25
Dashie <dashie@sigpipe.me>
2019-06-24 12:09:37 +0200

    Trying to unbreak this thing

--- front/src/modules/interface.js
+++ front/src/modules/interface.js
@@ -1,13 +1,34 @@
-const defaultState = {}
+import { set, delete as del } from 'vue'
+
+const defaultState = {
+  settings: {
+    currentSaveStateNotice: null,
+    noticeClearTimeout: null
+  }
+}
 
 const interfaceMod = {
   state: defaultState,
   mutations: {
-
+    settingsSaved (state, { success, error }) {
+      if (success) {
+        if (state.noticeClearTimeout) {
+          clearTimeout(state.noticeClearTimeout)
+        }
+        set(state.settings, 'currentSaveStateNotice', { error: false, data: success })
+        set(state.settings, 'noticeClearTimeout',
+          setTimeout(() => del(state.settings, 'currentSaveStateNotice'), 2000))
+      } else {
+        set(state.settings, 'currentSaveStateNotice', { error: true, errorData: error })
+      }
+    }
   },
   actions: {
     setPageTitle ({ rootState }, option = '') {
       document.title = `${option} ${rootState.instance.name}`
+    },
+    settingsSaved ({ commit, dispatch }, { success, error }) {
+      commit('settingsSaved', { success, error })
     }
   }
 }
