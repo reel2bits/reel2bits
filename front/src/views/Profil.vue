bc6f01b6ef7b30de122fc45b2a97ae7e8e77f69c
Dashie <dashie@sigpipe.me>
2019-06-24 11:39:32 +0200

    Small progress on oauth auto login from token

--- front/src/views/Profil.vue
+++ front/src/views/Profil.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>Profil page !!!</h1>
+    <h1>Profil page of {{currentUser.screen_name}}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    currentUser () { return this.$store.state.users.currentUser },
+  }
+}
+</script>
