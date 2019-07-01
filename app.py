58c7ff9091a47eb3fa3d7dce2d64c8009a874d36
Dashie <dashie@sigpipe.me>
2019-07-01 20:29:30 +0200

    meh

--- app.py
+++ app.py
@@ -4,17 +4,7 @@ import os
 import subprocess
 from logging.handlers import RotatingFileHandler
 from flask_babelex import gettext, Babel
-from flask import (
-    Flask,
-    render_template,
-    g,
-    send_from_directory,
-    jsonify,
-    safe_join,
-    request,
-    flash,
-    Response,
-)
+from flask import Flask, render_template, g, send_from_directory, jsonify, safe_join, request, flash, Response
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_migrate import Migrate
