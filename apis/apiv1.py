39f115ef2990a17f471341f2e121653a1c95f204
Dashie <dashie@sigpipe.me>
2019-07-01 16:20:42 +0200

    Start reverting flask_restful

--- apis/apiv1.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask_restplus import Api
-from .v1.accounts import api as api_accounts
-
-blueprint = Blueprint("api_v1", __name__, url_prefix="/api/v1")
-api = Api(blueprint, title="Api V1", version="1.0", description="Api V1")
-
-api.add_namespace(api_accounts)
