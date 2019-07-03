e4351e569bee89252a12abdd36ee731298f2c6c4
Dashie <dashie@sigpipe.me>
2019-07-03 20:19:16 +0200

    Redirect to right route

---
+++ front/src/views/tracks/Show.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h4>Show track</h4>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data: () => ({
+  }),
+  computed: {
+    ...mapState({
+      signedIn: state => !!state.users.currentUser
+    })
+  },
+  created () {
+  },
+  methods: {
+  }
+}
+</script>
