c8afe20f3a4c5f46de0177500bc4189fb8d2cee2
Dashie <dashie@sigpipe.me>
2019-07-03 19:59:23 +0200

    Can upload track

--- controllers/api/tracks.py
+++ controllers/api/tracks.py
@@ -2,9 +2,15 @@ from flask import Blueprint, request, jsonify
 from app_oauth import require_oauth
 from authlib.flask.oauth2 import current_token
 from forms import SoundUploadForm
+from models import db, Sound
 import json
+from utils import add_user_log, get_hashed_filename
+from flask_babelex import gettext
+from flask_uploads import UploadSet, AUDIO
 
 bp_api_tracks = Blueprint("bp_api_tracks", __name__)
+
+sounds = UploadSet("sounds", AUDIO)
 
 
 @bp_api_tracks.route("/api/tracks/upload", methods=["POST"])
@@ -29,6 +35,45 @@ def upload():
     form = SoundUploadForm()
 
     if form.validate_on_submit():
-        return jsonify('I have no idea what to do'), 500
+        filename_orig = request.files["file"].filename
+        filename_hashed = get_hashed_filename(filename_orig)
+
+        sounds.save(request.files["file"], folder=user.slug, name=filename_hashed)
+
+        rec = Sound()
+        rec.filename = filename_hashed
+        rec.filename_orig = filename_orig
+        rec.licence = form.licence.data
+        if form.album.data:
+            rec.album_id = form.album.data.id
+            if not form.album.data.sounds:
+                rec.album_order = 0
+            else:
+                rec.album_order = form.album.data.sounds.count() + 1
+
+        rec.user_id = user.id
+        if not form.title.data:
+            rec.title = filename_orig
+        else:
+            rec.title = form.title.data
+        rec.description = form.description.data
+        rec.private = form.private.data
+
+        if "flac" in request.files["file"].mimetype or "ogg" in request.files["file"].mimetype:
+            rec.transcode_state = Sound.TRANSCODE_WAITING
+            rec.transcode_needed = True
+
+        db.session.add(rec)
+        db.session.commit()
+
+        # push the job in queue
+        from tasks import upload_workflow
+
+        upload_workflow.delay(rec.id)
+
+        # log
+        add_user_log(rec.id, user.id, "sounds", "info", "Uploaded {0} -- {1}".format(rec.id, rec.title))
+
+        return jsonify({'message': gettext("Uploaded ! Processing will now follow."), 'id': rec.slug})
 
     return jsonify({'error': json.dumps(form.errors)}), 400
