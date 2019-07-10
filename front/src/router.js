7f128304dd36e6a663e1152e98b3d43c217efc94
Dashie <dashie@sigpipe.me>
2019-07-10 06:10:37 +0200

    Remove slug from API

--- front/src/router.js
+++ front/src/router.js
@@ -65,7 +65,7 @@ const router = new Router({
     },
     {
       name: 'tracks-show',
-      path: '/users/:username/track/:trackname',
+      path: '/tracks/:trackid',
       component: TracksShow
     }
   ]
