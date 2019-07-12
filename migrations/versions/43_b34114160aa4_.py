3c5fba1305c27ffd8e32600dda95f34af2623c08
Wilfried OLLIVIER <wollivier@bearstech.com>
2019-07-12 18:40:12 +0200

    Fix: make black happy 😊

--- migrations/versions/43_b34114160aa4_.py
+++ migrations/versions/43_b34114160aa4_.py
@@ -7,8 +7,8 @@ Create Date: 2019-07-12 18:12:16.228463
 """
 
 # revision identifiers, used by Alembic.
-revision = 'b34114160aa4'
-down_revision = '8b4238e4e1a3'
+revision = "b34114160aa4"
+down_revision = "8b4238e4e1a3"
 
 from models import db, Album  # noqa: E402
 from flake_id import gen_flakeid  # noqa: E402
