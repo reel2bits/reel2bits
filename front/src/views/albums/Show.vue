8ca2cf19d9db5710b059d663309d86d7967b322b
Wilfried OLLIVIER <wollivier@bearstech.com>
2019-07-14 23:01:05 +0200

    Add show album in frontend and backend

---
+++ front/src/views/albums/Show.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h4>Show album</h4>
+    <div v-if="isOwner">
+      <a
+        href="#"
+        @click.prevent="editAlbum"
+      >edit</a> | <a
+        href="#"
+        @click.prevent="deleteAlbum"
+      >delete</a>
+    </div>
+
+    <div v-if="errors">
+      {{ errors }}
+    </div>
+    <div>
+      Album to show: {{ album }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import apiService from '../../services/api/api.service.js'
+
+export default {
+  data: () => ({
+    album: null,
+    errors: null,
+    processing_done: null,
+    isOwner: false
+  }),
+  computed: {
+    ...mapState({
+      signedIn: state => !!state.users.currentUser
+    }),
+    albumId () {
+      return this.$route.params.albumId
+    },
+    userName () {
+      return this.$route.params.username
+    },
+    processingDone () {
+      return (this.processing_done && this.album)
+    }
+  },
+  created () {
+    this.fetchAlbum()
+  },
+  methods: {
+    async fetchAlbum () {
+      try {
+        let data = await apiService.albumFetch(this.userName, this.albumId, this.$store)
+        console.log(data)
+        this.album = data
+        this.isOwner = (this.album.user === this.$store.state.users.currentUser.screen_name)
+      } catch (e) {
+        this.errors = e.message
+      }
+    },
+    async editAlbum () {
+      console.log('want to edit album')
+    },
+    async deleteAlbum () {
+      console.log('want to delete album')
+    }
+  }
+}
+</script>
