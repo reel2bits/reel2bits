d386e44ac806c71130c6684c2e89e9bed5957714
Dashie <dashie@sigpipe.me>
2019-07-15 13:31:30 +0200

    Add tests for gen_flakeid

---
+++ tests/test_flake_id.py
@@ -0,0 +1,9 @@
+from flake_id import gen_flakeid
+import time
+
+def test_gen_flakeid():
+    for i in range(0, 100):
+        a = gen_flakeid()
+        b = gen_flakeid()
+        assert a != b
+        time.sleep(0.1)
