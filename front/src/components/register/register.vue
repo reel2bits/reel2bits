342bda31d9242fa9d074bc275972961b2adcc299
Dashie <dashie@sigpipe.me>
2019-08-22 23:50:30 +0200

    Start styling register; use vuelidate properly with bootstrap-vue

--- front/src/components/register/register.vue
+++ front/src/components/register/register.vue
@@ -1,107 +1,112 @@
 <template>
-  <div>
-    <h4>register</h4>
-    <form class="registration-form" @submit.prevent="register(user)">
-      <div class="container">
-        <div class="text-fields">
-          <div class="form-group" :class="{ 'form-group--error': $v.user.username.$error }">
-            <label class="form--label" for="sign-up-username">username</label>
-            <input id="sign-up-username" v-model.trim="$v.user.username.$model" :disabled="isPending"
-                   class="form-control" placeholder="username"
-            >
-          </div>
-          <div v-if="$v.user.username.$dirty" class="form-error">
-            <ul>
-              <li v-if="!$v.user.username.required">
-                <span>username required</span>
-              </li>
-            </ul>
-          </div>
+  <div class="row justify-content-md-center">
+    <div class="col-md-4">
+      <h4>Registering</h4>
 
-          <div class="form-group" :class="{ 'form-group--error': $v.user.fullname.$error }">
-            <label class="form--label" for="sign-up-fullname">full name</label>
-            <input id="sign-up-fullname" v-model.trim="$v.user.fullname.$model" :disabled="isPending"
-                   class="form-control" placeholder="foo bar"
-            >
-          </div>
-          <div v-if="$v.user.fullname.$dirty" class="form-error">
-            <ul>
-              <li v-if="!$v.user.fullname.required">
-                <span>full name required</span>
-              </li>
-            </ul>
-          </div>
+      <b-form class="registration-form" @submit.prevent="register(user)">
+        <b-form-group
+          id="ig-username"
+          :class="{ 'form-group--error': $v.user.username.$error }"
+          label="Username:"
+          label-for="username"
+        >
+          <b-form-input
+            id="username"
+            v-model.trim="$v.user.username.$model"
+            :disabled="isPending"
+            placeholder="username"
+            :state="$v.user.username.$dirty ? !$v.user.username.$error : null"
+            aria-describedby="username-live-feedback"
+          />
+          <b-form-invalid-feedback id="username-live-feedback">
+            Username required
+          </b-form-invalid-feedback>
+        </b-form-group>
 
-          <div class="form-group" :class="{ 'form-group--error': $v.user.email.$error }">
-            <label class="form--label" for="email">email</label>
-            <input id="email" v-model="$v.user.email.$model" :disabled="isPending"
-                   class="form-control" type="email"
-            >
-          </div>
-          <div v-if="$v.user.email.$dirty" class="form-error">
-            <ul>
-              <li v-if="!$v.user.email.required">
-                <span>email required</span>
-              </li>
-            </ul>
-          </div>
-
-          <div class="form-group">
-            <label class="form--label" for="bio">bio (optional)</label>
-            <textarea
-              id="bio"
-              v-model="user.bio"
-              :disabled="isPending"
-              class="form-control"
-              :placeholder="bioPlaceholder"
-            />
-          </div>
-
-          <div class="form-group" :class="{ 'form-group--error': $v.user.password.$error }">
-            <label class="form--label" for="sign-up-password">password</label>
-            <input id="sign-up-password" v-model="user.password" :disabled="isPending"
-                   class="form-control" type="password"
-            >
-          </div>
-          <div v-if="$v.user.password.$dirty" class="form-error">
-            <ul>
-              <li v-if="!$v.user.password.required">
-                <span>password required</span>
-              </li>
-            </ul>
-          </div>
-
-          <div class="form-group" :class="{ 'form-group--error': $v.user.confirm.$error }">
-            <label class="form--label" for="sign-up-password-confirmation">confirm password</label>
-            <input id="sign-up-password-confirmation" v-model="user.confirm" :disabled="isPending"
-                   class="form-control" type="password"
-            >
-          </div>
-          <div v-if="$v.user.confirm.$dirty" class="form-error">
-            <ul>
-              <li v-if="!$v.user.confirm.required">
-                <span>password confirmation required</span>
-              </li>
-              <li v-if="!$v.user.confirm.sameAsPassword">
-                <span>password and confirmation needs to match</span>
-              </li>
-            </ul>
-          </div>
-          <div class="form-group">
-            <button :disabled="isPending" type="submit" class="btn btn-default">
-              submit
-            </button>
-          </div>
+        <div class="form-group" :class="{ 'form-group--error': $v.user.fullname.$error }">
+          <label class="form--label" for="sign-up-fullname">Display name:</label>
+          <input id="sign-up-fullname" v-model.trim="$v.user.fullname.$model" :disabled="isPending"
+                 class="form-control" placeholder="foo bar"
+          >
+        </div>
+        <div v-if="$v.user.fullname.$dirty" class="form-error">
+          <ul>
+            <li v-if="!$v.user.fullname.required">
+              <span>full name required</span>
+            </li>
+          </ul>
         </div>
 
-        <div class="terms-of-service" v-html="termsOfService" />
-      </div>
-      <div v-if="serverValidationErrors" class="form-group">
-        <div class="alert error">
-          <span v-for="error in serverValidationErrors" :key="error">{{ error }}</span>
+        <div class="form-group" :class="{ 'form-group--error': $v.user.email.$error }">
+          <label class="form--label" for="email">Email:</label>
+          <input id="email" v-model="$v.user.email.$model" :disabled="isPending"
+                 class="form-control" type="email"
+          >
         </div>
-      </div>
-    </form>
+        <div v-if="$v.user.email.$dirty" class="form-error">
+          <ul>
+            <li v-if="!$v.user.email.required">
+              <span>email required</span>
+            </li>
+          </ul>
+        </div>
+
+        <div class="form-group">
+          <label class="form--label" for="bio">bio (optional):</label>
+          <textarea
+            id="bio"
+            v-model="user.bio"
+            :disabled="isPending"
+            class="form-control"
+            :placeholder="bioPlaceholder"
+          />
+        </div>
+
+        <div class="form-group" :class="{ 'form-group--error': $v.user.password.$error }">
+          <label class="form--label" for="sign-up-password">Password:</label>
+          <input id="sign-up-password" v-model="user.password" :disabled="isPending"
+                 class="form-control" type="password"
+          >
+        </div>
+        <div v-if="$v.user.password.$dirty" class="form-error">
+          <ul>
+            <li v-if="!$v.user.password.required">
+              <span>password required</span>
+            </li>
+          </ul>
+        </div>
+
+        <div class="form-group" :class="{ 'form-group--error': $v.user.confirm.$error }">
+          <label class="form--label" for="sign-up-password-confirmation">Confirm password:</label>
+          <input id="sign-up-password-confirmation" v-model="user.confirm" :disabled="isPending"
+                 class="form-control" type="password"
+          >
+        </div>
+        <div v-if="$v.user.confirm.$dirty" class="form-error">
+          <ul>
+            <li v-if="!$v.user.confirm.required">
+              <span>password confirmation required</span>
+            </li>
+            <li v-if="!$v.user.confirm.sameAsPassword">
+              <span>password and confirmation needs to match</span>
+            </li>
+          </ul>
+        </div>
+        <div class="form-group">
+          <b-button :disabled="isPending" type="submit" variant="primary">
+            Register
+          </b-button>
+        </div>
+      </b-form>
+
+      <b-alert v-if="serverValidationErrors.length > 0" variant="danger" show>
+        <span v-for="error in serverValidationErrors" :key="error">{{ error }}</span>
+      </b-alert>
+    </div>
+
+    <div class="col-md-4">
+      <div class="terms-of-service" v-html="termsOfService" />
+    </div>
   </div>
 </template>
 
