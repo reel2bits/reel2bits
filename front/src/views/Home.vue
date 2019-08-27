a3907c08a9d3f640e4718f29defd1bd2ed53197c
Dashie <dashie@sigpipe.me>
2019-08-27 17:17:53 +0200

    correctly stop fetching timeline

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -7,6 +7,13 @@
 <script>
 // @ is an alias to /src
 
+// TODO: this will be reworked, this shouldn't be friends timeline
 export default {
+  created () {
+    this.$store.dispatch('startFetchingTimeline', { timeline: 'friends' })
+  },
+  destroyed () {
+    this.$store.dispatch('stopFetching', 'friends')
+  }
 }
 </script>
