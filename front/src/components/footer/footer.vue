441f4542eda0cfdfc6191e47415335a3bd3912eb
Dashie <dashie@sigpipe.me>
2019-08-28 18:11:56 +0200

    Widen the footer columns, add title to url label

--- front/src/components/footer/footer.vue
+++ front/src/components/footer/footer.vue
@@ -1,20 +1,20 @@
 <template>
   <footer class="text-center">
     <div class="row justify-content-around">
-      <div class="col-md-3 text-truncate">
+      <div class="col-md-4 text-truncate" :title="url">
         {{ url }}
       </div>
-      <div class="col-md-3">
+      <div class="col-md-4">
         About
       </div>
     </div>
     <div class="row justify-content-around">
-      <div class="col-md-3">
+      <div class="col-md-4">
         <router-link to="/about">
           About
         </router-link>
       </div>
-      <div class="col-md-3">
+      <div class="col-md-4">
         <a :href="sourceUrl" target="_blank">Source code</a>
       </div>
     </div>
