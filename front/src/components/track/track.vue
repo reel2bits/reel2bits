4fe224af8ecfe56328cb4982497dc122f79413c5
Dashie <dashie@sigpipe.me>
2019-09-01 15:35:31 +0200

    Fix tracks not being normalized in timelines; Change /userName/trackId route

--- front/src/components/track/track.vue
+++ front/src/components/track/track.vue
@@ -2,8 +2,10 @@
   <div>
     <div class="row">
       <div class="col-md-12">
-        Track: {{ track.reel2bits.title }}<br>
-        Published on: {{ track.created_at }}
+        Track: <b-link :to="{ name: 'tracks-show', params: { username: track.account.screen_name, trackId: track.slug } }">
+          {{ track.title }}
+        </b-link><br>
+        Published on: {{ track.uploaded_on }}
         <hr>
       </div>
     </div>
