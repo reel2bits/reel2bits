4a828aa49b80cb84c5cc34e5cda9c3a3646b5e99
Dashie <dashie@sigpipe.me>
2019-09-03 12:50:20 +0200

    Extract the timeline tabs in component; Use nav tabs

---
+++ front/src/components/timeline_tabs/timeline_tabs.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-nav tabs>
+    <b-nav-item :active="active == 'friends'" :to="{ name: 'friends' }">
+      Your feed
+    </b-nav-item>
+    <b-nav-item :active="active == 'public'" :to="{ name: 'public-timeline' }">
+      Public
+    </b-nav-item>
+    <b-nav-item :active="active == 'publicAndExternal'" :to="{ name: 'public-external-timeline' }">
+      The Whole Known Network
+    </b-nav-item>
+  </b-nav>
+</template>
+
+<script>
+const TimelineTabs = {
+  props: [
+    'active'
+  ]
+}
+
+export default TimelineTabs
+</script>
