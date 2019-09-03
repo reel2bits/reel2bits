745c04ae6e53c6d8923b868c65427246758ebfa3
Dashie <dashie@sigpipe.me>
2019-09-03 23:19:52 +0200

    Add first part of reset password workflow

--- front/src/boot/routes.js
+++ front/src/boot/routes.js
@@ -14,6 +14,7 @@ import AlbumsShow from '../views/albums/Show.vue'
 import AccountLogs from '../views/account/Logs.vue'
 import About from '../views/About.vue'
 import NotFound from '../views/NotFound.vue'
+import PasswordReset from '../components/password_reset/password_reset.vue'
 
 export default (store) => {
   const validateAuthenticatedRoute = (to, from, next) => {
@@ -40,6 +41,7 @@ export default (store) => {
     { path: '/about', name: 'about', component: About },
     { path: '/login', name: 'login_form', component: LoginForm },
     { path: '/register', name: 'register', component: Register },
+    { name: 'password-reset', path: '/password-reset', component: PasswordReset },
     { name: 'oauth-callback', path: '/oauth-callback', component: OAuthCallback, props: (route) => ({ code: route.query.code }) },
     { name: 'external-user-profile', path: '/users/:id', component: UserProfile },
     { name: 'user-profile', path: '/(users/)?:name', component: UserProfile },
---
+++ front/src/components/password_reset/password_reset.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="row justify-content-md-center">
+    <div class="col-md-3">
+      <b-form class="password-reset-form" @submit.prevent="submit">
+        <h1>Reset password</h1>
+        <b-form-group
+          id="ig-email"
+          label="Email:"
+          label-for="email"
+        >
+          <b-form-input
+            id="email"
+            ref="email"
+            v-model="user.email"
+            type="text"
+            placeholder="Enter email"
+            :disabled="isPending"
+          />
+        </b-form-group>
+
+        <b-button type="submit" variant="primary" :disabled="isPending">
+          Login
+        </b-button>
+      </b-form>
+
+      <br>
+      <b-alert v-if="error" variant="danger" show
+               @dismissed="error = null"
+      >
+        {{ error }}
+      </b-alert>
+      <b-alert v-if="success" variant="success" show>
+        Link generated with success, check your emails.
+      </b-alert>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+const passwordReset = {
+  data: () => ({
+    user: {
+      email: ''
+    },
+    isPending: false,
+    success: false,
+    error: null
+  }),
+  computed: {
+    ...mapState({
+      signedIn: (state) => !!state.users.currentUser
+    })
+  },
+  created () {
+    if (this.signedIn) {
+      this.$router.push({ name: 'root' })
+    }
+  },
+  methods: {
+    submit () {
+      this.isPending = true
+      const email = this.user.email
+      this.$store.state.api.backendInteractor.resetPassword({ email })
+        .then(({ status }) => {
+          this.isPending = false
+          this.user.email = ''
+
+          if (status === 204) {
+            this.success = true
+            this.error = null
+          } else if (status === 404 || status === 400) {
+            this.error = 'Cannot generate reset link'
+            this.$nextTick(() => {
+              this.$refs.email.focus()
+            })
+          }
+        })
+        .catch(() => {
+          this.isPending = false
+          this.user.email = ''
+          this.error = 'Cannot generate reset link'
+        })
+    }
+  }
+}
+
+export default passwordReset
+</script>
