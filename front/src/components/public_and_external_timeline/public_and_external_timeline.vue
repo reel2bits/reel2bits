24f790730a5f1749ce122f907eef2038c14ed753
Dashie <dashie@sigpipe.me>
2019-09-05 23:03:13 +0200

    Show footer on timelines

--- front/src/components/public_and_external_timeline/public_and_external_timeline.vue
+++ front/src/components/public_and_external_timeline/public_and_external_timeline.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>
-    <TimelineTabs active="publicAndExternal" />
+  <div class="row">
+    <div class="col-md-8">
+      <TimelineTabs active="publicAndExternal" />
 
-    <Timeline
-      title="The Whole Known Network"
-      :timeline-name="'publicAndExternal'"
-    />
+      <Timeline
+        title="The Whole Known Network"
+        :timeline-name="'publicAndExternal'"
+      />
+    </div>
+    <div class="col-md-4">
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
 import TimelineTabs from '../timeline_tabs/timeline_tabs.vue'
 import Timeline from '../timeline/timeline.vue'
+import Footer from '../footer/footer.vue'
 const PublicAndExternalTimeline = {
   components: {
     TimelineTabs,
-    Timeline
+    Timeline,
+    Footer
   }
 }
 
--- front/src/components/public_timeline/public_timeline.vue
+++ front/src/components/public_timeline/public_timeline.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>
-    <TimelineTabs active="public" />
+  <div class="row">
+    <div class="col-md-8">
+      <TimelineTabs active="public" />
 
-    <Timeline
-      title="Public Timeline"
-      timeline-name="public"
-    />
+      <Timeline
+        title="Public Timeline"
+        timeline-name="public"
+      />
+    </div>
+    <div class="col-md-4">
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
 import TimelineTabs from '../timeline_tabs/timeline_tabs.vue'
 import Timeline from '../timeline/timeline.vue'
+import Footer from '../footer/footer.vue'
 const PublicTimeline = {
   components: {
     TimelineTabs,
-    Timeline
+    Timeline,
+    Footer
   }
 }
 
