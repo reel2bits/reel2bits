800df8a64f9abc1380a97bde654fb5ec50e2daa5
Dashie <dashie@sigpipe.me>
2019-09-05 23:03:13 +0200

    Show footer on timelines

--- front/src/components/friends_timeline/friends_timeline.vue
+++ front/src/components/friends_timeline/friends_timeline.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>
-    <TimelineTabs active="friends" />
+  <div class="row">
+    <div class="col-md-8">
+      <TimelineTabs active="friends" />
 
-    <Timeline
-      title="Timeline"
-      timeline-name="friends"
-    />
+      <Timeline
+        title="Timeline"
+        timeline-name="friends"
+      />
+    </div>
+    <div class="col-md-4">
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
 import TimelineTabs from '../timeline_tabs/timeline_tabs.vue'
 import Timeline from '../timeline/timeline.vue'
+import Footer from '../footer/footer.vue'
 const FriendsTimeline = {
   components: {
     TimelineTabs,
-    Timeline
+    Timeline,
+    Footer
   }
 }
 
