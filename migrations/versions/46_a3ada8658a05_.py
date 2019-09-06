a148f0f2e980bcd31776df8409b68758659c4de1
Dashie <dashie@sigpipe.me>
2019-09-06 13:26:42 +0200

    Start adding follow user and unfollow

---
+++ migrations/versions/46_a3ada8658a05_.py
@@ -0,0 +1,25 @@
+"""Set User.local == user.actor.local
+
+Revision ID: a3ada8658a05
+Revises: fc79fead3906
+Create Date: 2019-09-06 13:18:57.269963
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "a3ada8658a05"
+down_revision = "fc79fead3906"
+
+from models import db, User  # noqa: E402
+
+
+def upgrade():
+    for user in db.session.query(User).all():
+        if len(user.actor) >= 1:
+            if user.actor[0]:
+                user.local = user.actor[0].is_local()
+    db.session.commit()
+
+
+def downgrade():
+    pass
