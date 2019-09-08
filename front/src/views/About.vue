9d364fb3db6b70ca24d0e9b82677516c053a1cbb
Dashie <dashie@sigpipe.me>
2019-09-08 20:55:29 +0200

    translate About

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="row justify-content-md-center">
     <div class="col-md-8">
-      <h2>Welcome on instance: {{ instanceName }}</h2>
+      <h2 v-translate="{instanceName: instanceName}" translate-context="Content/About/Headline/Title">
+        Welcome on instance: %{ instanceName }
+      </h2>
       <div>
-        <h3>Description</h3>
+        <h3 v-translate translate-context="Content/About/Headline/Description">
+          Description
+        </h3>
         {{ instanceDescription }}
       </div>
       <div v-html="instanceTos" />
       <div>
-        <h3>Versions and sources</h3>
-        Frontend version: {{ frontVersion }}<br>
-        Backend version: {{ backendVersion }}<br>
-        Source: <a :href="sourceUrl" target="_blank">{{ sourceUrl }}</a>
+        <h3 v-translate translate-context="Content/About/Headline/VersAndSources">
+          Versions and sources
+        </h3>
+        <translate translate-context="Content/About/Headline/FrontendVersion" :translate-params="{frontVersion: frontVersion}">
+          Frontend version: %{ frontVersion }
+        </translate><br>
+        <translate translate-context="Content/About/Headline/BackendVersion" :translate-params="{backendVersion: backendVersion}">
+          Backend version: %{ backendVersion }
+        </translate><br>
+        <translate translate-context="Content/About/Link/Prefix">
+          Source:
+        </translate><a :href="sourceUrl" target="_blank">{{ sourceUrl }}</a>
       </div>
     </div>
   </div>
