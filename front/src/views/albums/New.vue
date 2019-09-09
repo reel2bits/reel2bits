a704e354c0fc5c0ef63e2f1af2710d7f0000deec
Dashie <dashie@sigpipe.me>
2019-09-09 18:12:17 +0200

    translate the rest

--- front/src/views/albums/New.vue
+++ front/src/views/albums/New.vue
@@ -1,55 +1,61 @@
 <template>
   <div class="row justify-content-md-center">
     <div class="col-md-6">
-      <h4>Create a new album</h4>
+      <h4 v-translate translate-context="Content/AlbumNew/Headline">
+        Create a new album
+      </h4>
       <b-form class="albums-new-form" @submit.prevent="create(album)">
         <b-form-group
           id="ig-title"
           :class="{ 'form-group--error': $v.album.title.$error }"
-          label="Title:"
+          :label="labels.titleLabel"
           label-for="title"
         >
           <b-form-input
             id="title"
             v-model.trim="$v.album.title.$model"
             :disabled="isPending"
-            placeholder="title"
+            :placeholder="labels.titlePlaceholder"
             :state="$v.album.title.$dirty ? !$v.album.title.$error : null"
             aria-describedby="title-live-feedback"
           />
           <b-form-invalid-feedback id="title-live-feedback">
-            <span v-if="!$v.album.title.required">A title is required</span>
-            <span v-if="!$v.album.title.maxLength">Length is limited to 250 characters</span>
+            <span v-if="!$v.album.title.required" v-translate translate-context="Content/AlbumNew/Feedback/Title/Required">A title is required</span>
+            <span v-if="!$v.album.title.maxLength" v-translate translate-context="Content/AlbumNew/Feedback/Title/LengthLimit">Length is limited to 250 characters</span>
           </b-form-invalid-feedback>
         </b-form-group>
 
         <b-form-group
           id="ig-description"
-          label="Description:"
+          :label="labels.descriptionLabel"
           label-for="description"
         >
           <b-form-textarea
             id="description"
             v-model="album.description"
             :disabled="isPending"
-            :placeholder="descriptionPlaceholder"
+            :placeholder="labels.descriptionPlaceholder"
           />
         </b-form-group>
 
         <b-form-checkbox
           id="private"
           v-model="album.private"
+          v-translate
           name="private"
           value="y"
           unchecked-value=""
           :disabled="isPending"
+          translate-context="Content/AlbumNew/Input.Label/Private album"
         >
           this album is private
         </b-form-checkbox>
 
         <br>
 
-        <b-button :disabled="isPending" type="submit" variant="primary">
+        <b-button v-translate :disabled="isPending" type="submit"
+                  variant="primary" translate-context="Content/AlbumNew/Button/Create"
+        >
           Create
         </b-button>
 
@@ -86,14 +92,19 @@ export default {
     }
   },
   computed: {
-    descriptionPlaceholder () {
-      return 'Optional, what is this album about ?'
-    },
     ...mapState({
       signedIn: state => !!state.users.currentUser,
       isPending: state => state.albums.createPending,
       serverValidationErrors: state => state.albums.createErrors
-    })
+    }),
+    labels () {
+      return {
+        titleLabel: this.$pgettext('Content/AlbumNew/Input.Label/Email', 'Title:'),
+        titlePlaceholder: this.$pgettext('Content/AlbumNew/Input.Placeholder/Title', 'Your album title.'),
+        descriptionLabel: this.$pgettext('Content/AlbumNew/Input.Label/Description', 'Description:'),
+        descriptionPlaceholder: this.$pgettext('Content/AlbumNew/Input.Placeholder/Description', 'Optional, what is this album about ?')
+      }
+    }
   },
   methods: {
     ...mapActions(['albumNew']),
