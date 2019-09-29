91601c62f41c2191021faecd0c9ca4f91153b1df
Dashie <dashie@sigpipe.me>
2019-09-29 19:19:32 +0200

    Disable this one too

--- api/migrations/versions/41_7eb56606e9d6_.py
+++ api/migrations/versions/41_7eb56606e9d6_.py
@@ -10,16 +10,21 @@ Create Date: 2019-07-06 07:55:00.474793
 revision = "7eb56606e9d6"
 down_revision = "a8e2274a18d3"
 
-from models import db, Sound  # noqa: E402
-from utils.flake_id import gen_flakeid  # noqa: E402
+# from utils.flake_id import gen_flakeid  # noqa: E402
 from uuid import UUID  # noqa: E402
+from sqlalchemy.sql import table, column  # noqa: E402
+
+# from alembic import op  # noqa: E402
+
+
+sound = table("sound", column("flake_id", UUID(as_uuid=True)))
 
 
 def upgrade():
-    for sound in db.session.query(Sound).all():
-        if not sound.flake_id:
-            sound.flake_id = UUID(int=gen_flakeid())
-    db.session.commit()
+    # op.execute(
+    #     sound.update().where(sound.flake_id.is_(None)).values({'flake_id': UUID(int=gen_flakeid())})
+    # )
+    pass
 
 
 def downgrade():
