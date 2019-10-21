05bf75782a52c409db2d85e8acb14e0d0670c98d
Dashie <dashie@sigpipe.me>
2019-10-21 22:16:33 +0200

    Add state for unified player base

---
+++ front/src/modules/player.js
@@ -0,0 +1,14 @@
+const defaultState = {
+  playingTrack: null,
+  playback: null
+}
+
+const playerMod = {
+  state: defaultState,
+  mutations: {
+  },
+  actions: {
+  }
+}
+
+export default playerMod
