3583599e157ae16cb562066d350851effab54685
Dashie <dashie@sigpipe.me>
2019-10-31 21:53:14 +0100

    Reintroduce the old search api endpoint

--- api/controllers/api/reel2bits.py
+++ api/controllers/api/reel2bits.py
@@ -1,15 +1,23 @@
 from flask import Blueprint, jsonify, request, abort, current_app, render_template
-from models import db, User, PasswordResetToken, Sound, SoundTag
+from models import db, User, PasswordResetToken, Sound, SoundTag, Actor, Follower
 from utils.various import add_user_log, generate_random_token, add_log
 from app_oauth import require_oauth
 from authlib.flask.oauth2 import current_token
 from flask_security.utils import hash_password, verify_password
 from flask_mail import Message
 import smtplib
+import re
 from utils.defaults import Reel2bitsDefaults
 from datas_helpers import default_genres
+from little_boxes.webfinger import get_actor_url
+from little_boxes.urlutils import InvalidURLError
+from little_boxes import activitypub as ap
+from urllib.parse import urlparse
+from sqlalchemy import or_, and_, not_
 
 bp_api_reel2bits = Blueprint("bp_api_reel2bits", __name__)
+
+RE_ACCOUNT = re.compile(r"^(?P<user>[\w]+)@(?P<instance>[\w.]+)$")
 
 
 @bp_api_reel2bits.route("/api/reel2bits/licenses", methods=["GET"])
@@ -239,3 +247,174 @@ def reset_password_token(token):
     add_user_log(tok.user.id, tok.user.id, "user", "info", "Password have been changed")
 
     return jsonify({"status": "success"}), 204
+
+
+@bp_api_reel2bits.route("/api/reel2bits/search", methods=["GET"])
+@require_oauth(optional=True)
+def search():
+    """
+    Search.
+    ---
+    tags:
+        - Global
+    parameters:
+        - name: q
+          in: query
+          type: string
+          required: true
+          description: search string
+    responses:
+        200:
+            description: fixme.
+    """
+    # Get logged in user from bearer token, or None if not logged in
+    if current_token:
+        current_user = current_token.user
+    else:
+        current_user = None
+
+    s = request.args.get("q", None)
+    if not s:
+        return jsonify({"error": "No search string provided"}), 400
+
+    # This is the old search endpoint and needs to be improved
+
+    results = {"accounts": [], "sounds": [], "mode": None, "from": None}
+
+    if current_user:
+        results["from"] = current_user.name
+
+    # Search for sounds
+    # TODO: Implement FTS to get sounds search
+
+    # Search for accounts
+    accounts = []
+    is_user_at_account = RE_ACCOUNT.match(s)
+
+    if s.startswith("https://"):
+        results["mode"] = "uri"
+        if current_user:
+            users = (
+                db.session.query(Actor, Follower)
+                .outerjoin(
+                    Follower, and_(Actor.id == Follower.target_id, Follower.actor_id == current_user.actor[0].id)
+                )
+                .filter(Actor.url == s)
+                .filter(not_(Actor.id == current_user.actor[0].id))
+                .all()
+            )
+        else:
+            users = db.session.query(Actor).filter(Actor.url == s).all()
+    elif is_user_at_account:
+        results["mode"] = "acct"
+        user = is_user_at_account.group("user")
+        instance = is_user_at_account.group("instance")
+        if current_user:
+            users = (
+                db.session.query(Actor, Follower)
+                .outerjoin(
+                    Follower, and_(Actor.id == Follower.target_id, Follower.actor_id == current_user.actor[0].id)
+                )
+                .filter(Actor.preferred_username == user, Actor.domain == instance)
+                .filter(not_(Actor.id == current_user.actor[0].id))
+                .all()
+            )
+        else:
+            users = db.session.query(Actor).filter(Actor.preferred_username == user, Actor.domain == instance).all()
+    else:
+        results["mode"] = "username"
+        # Match actor username in database
+        if current_user:
+            users = (
+                db.session.query(Actor, Follower)
+                .outerjoin(
+                    Follower, and_(Actor.id == Follower.target_id, Follower.actor_id == current_user.actor[0].id)
+                )
+                .filter(or_(Actor.preferred_username.contains(s), Actor.name.contains(s)))
+                .filter(not_(Actor.id == current_user.actor[0].id))
+                .all()
+            )
+        else:
+            users = (
+                db.session.query(Actor).filter(or_(Actor.preferred_username.contains(s), Actor.name.contains(s))).all()
+            )
+
+    # Handle the results
+    if len(users) > 0:
+        for user in users:
+            if current_user:
+                if user[1]:
+                    follows = user[1].actor_id == current_user.actor[0].id
+                else:
+                    follows = False
+            else:
+                follows = None
+
+            if type(user) is Actor:
+                # Unauthenticated results
+                accounts.append(
+                    {
+                        "username": user.name,
+                        "name": user.preferred_username,
+                        "summary": user.summary,
+                        "instance": user.domain,
+                        "url": user.url,
+                        "remote": not user.is_local(),
+                        "follow": follows,
+                    }
+                )
+            else:
+                accounts.append(
+                    {
+                        "username": user[0].name,
+                        "name": user[0].preferred_username,
+                        "summary": user[0].summary,
+                        "instance": user[0].domain,
+                        "url": user[0].url,
+                        "remote": not user[0].is_local(),
+                        "follow": follows,
+                    }
+                )
+
+    if len(accounts) <= 0:
+        # Do a webfinger
+        current_app.logger.debug(f"webfinger for {s}")
+        try:
+            remote_actor_url = get_actor_url(s, debug=current_app.debug)
+            # We need to get the remote Actor
+            backend = ap.get_backend()
+            iri = backend.fetch_iri(remote_actor_url)
+            if iri:
+                current_app.logger.debug(f"got remote actor URL {remote_actor_url}")
+                # Fixme handle unauthenticated users plus duplicates follows
+                follow_rel = (
+                    db.session.query(Actor.id, Follower.id)
+                    .outerjoin(Follower, Actor.id == Follower.target_id)
+                    .filter(Actor.url == remote_actor_url)
+                    .first()
+                )
+                if follow_rel:
+                    follow_status = follow_rel[1] is not None
+                else:
+                    follow_status = False
+
+                domain = urlparse(iri["url"])
+                user = {
+                    "username": iri["name"],
+                    "name": iri["preferredUsername"],
+                    "instance": domain.netloc,
+                    "url": iri["url"],
+                    "remote": True,
+                    "summary": iri["summary"],
+                    "follow": follow_status,
+                }
+                accounts.append(user)
+                results["mode"] = "webfinger"
+                # Use iri to populate results["accounts"]
+        except (InvalidURLError, ValueError):
+            current_app.logger.exception(f"Invalid webfinger URL: {s}")
+
+    # Finally fill the results dict
+    results["accounts"] = accounts
+
+    return jsonify({"who": s, "results": results})
