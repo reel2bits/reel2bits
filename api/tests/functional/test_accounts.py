5d37a0648e70fdca2b6ea385814cd42c5681716e
Dashie <dashie@sigpipe.me>
2019-12-17 08:39:22 +0100

    Finish covering api/v1/accounts.py

--- api/tests/functional/test_accounts.py
+++ api/tests/functional/test_accounts.py
@@ -98,20 +98,48 @@ def test_user_statuses_empty(client, session):
     assert resp.json["totalItems"] == 0
 
 
+def test_relationships_empty(client, session):
+    """
+    Test user relationships (empty set)
+    /api/v1/account/relationships
+    """
+    # Login as user A
+    client_id, client_secret, access_token = login(client, "testusera", "testusera")
+
+    # Get relationships, should be empty
+    resp = client.get(f"/api/v1/accounts/relationships/", headers=headers(access_token))
+    assert resp.status_code == 200
+    assert isinstance(resp.json, list)
+    assert len(resp.json) == 0
+
+
 def test_follow(client, session):
     """
     Test follow
     /api/v1/accounts/<username_or_id>/follow
-    """
-    pass
 
+    user A follow user B
+    """
+    # Login as user A
+    client_id, client_secret, access_token = login(client, "testusera", "testusera")
 
-def test_relationships(client, session):
-    """
-    Test user relationships
-    /api/v1/account/relationships
-    """
-    pass
+    # then follow user B
+    resp = client.post("/api/v1/accounts/testuserb/follow", headers=headers(access_token))
+    assert resp.status_code in [200, 202]  # local, remote
+
+    # get userb flake_id
+    userB = User.query.filter(User.name == "testuserb").first()
+    assert userB.flake_id
+    # get relationship between A and B
+    resp = client.get(f"/api/v1/accounts/relationships/?id={userB.flake_id}", headers=headers(access_token))
+    assert resp.status_code == 200
+    assert isinstance(resp.json, list)
+    rel = resp.json[0]
+    assert rel["id"] != userB.flake_id
+    assert rel["following"] is True
+    assert rel["followed_by"] is False  # not yet followed
+    assert rel["blocking"] is False
+    assert rel["muting"] is False
 
 
 def test_followers(client, session):
@@ -119,15 +147,41 @@ def test_followers(client, session):
     Test followers list
     /api/v1/accounts/<username_or_id>/followers
     """
-    pass
+    # A follow B, A has no followers, B has one follower
+    # get followers for user A
+    resp = client.get("/api/v1/accounts/testusera/followers", headers=headers())
+    assert resp.status_code == 200
+    assert resp.json["page"] == 1
+    assert resp.json["totalItems"] == 0
+    assert len(resp.json["items"]) == 0
+
+    # get followers for user B
+    resp = client.get("/api/v1/accounts/testuserb/followers", headers=headers())
+    assert resp.status_code == 200
+    assert resp.json["page"] == 1
+    assert resp.json["totalItems"] == 1
+    assert len(resp.json["items"]) == 1
 
 
 def test_followings(client, session):
     """
     Test followings list
-    /api/v1/accounts/<username_or_id>/followings
+    /api/v1/accounts/<username_or_id>/following
     """
-    pass
+    # A follow B, A has one following, B has no followings
+    # get followers for user A
+    resp = client.get("/api/v1/accounts/testusera/following", headers=headers())
+    assert resp.status_code == 200
+    assert resp.json["page"] == 1
+    assert resp.json["totalItems"] == 1
+    assert len(resp.json["items"]) == 1
+
+    # get followers for user B
+    resp = client.get("/api/v1/accounts/testuserb/following", headers=headers())
+    assert resp.status_code == 200
+    assert resp.json["page"] == 1
+    assert resp.json["totalItems"] == 0
+    assert len(resp.json["items"]) == 0
 
 
 def test_unfollow(client, session):
@@ -135,15 +189,27 @@ def test_unfollow(client, session):
     Test unfollow
     /api/v1/accounts/<username_or_id>/unfollow
     """
-    pass
+    # login as user A
+    client_id, client_secret, access_token = login(client, "testusera", "testusera")
 
+    # then unfollow user B
+    resp = client.post("/api/v1/accounts/testuserb/unfollow", headers=headers(access_token))
+    assert resp.status_code in [200, 202]  # local and remote
 
-def test_relationships_none(client, session):
-    """
-    Test user relationships
-    /api/v1/account/relationships
-    """
-    pass
+    # check relationship
+    # get userb flake_id
+    userB = User.query.filter(User.name == "testuserb").first()
+    assert userB.flake_id
+    # get relationship between A and B
+    resp = client.get(f"/api/v1/accounts/relationships/?id={userB.flake_id}", headers=headers(access_token))
+    assert resp.status_code == 200
+    assert isinstance(resp.json, list)
+    rel = resp.json[0]
+    assert rel["id"] != userB.flake_id
+    assert rel["following"] is False
+    assert rel["followed_by"] is False
+    assert rel["blocking"] is False
+    assert rel["muting"] is False
 
 
 def test_account_delete(client, session):
